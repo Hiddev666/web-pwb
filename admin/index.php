9e6a95df8f88bb2f41ca2eb173d83f02f2157a5e
hiddev <officehiddev@gmail.com>
2023-08-15 10:42:38 +0700

    part 2 is end

--- admin/index.php
+++ admin/index.php
@@ -28,24 +28,25 @@ include("../config.php");
         <form action="" method="POST" class="row g-3 mt-4">
             <div class="col-4">
                 <?php
-                $katakunci = "";                
-                if(isset($_POST['submit'])) {
+                $katakunci = "";
+                if (isset($_POST['submit'])) {
                     $katakunci = $_POST['katakunci'];
                 }
 
 
-                if(isset($_GET['operation'])) {
+                if (isset($_GET['operation'])) {
                     $operation = $_GET['operation'];
                     $id = $_GET['id'];
 
                     $query = "DELETE FROM halaman where id='$id'";
                     $sql = mysqli_query($db, $query);
-                } 
+                }
 
 
 
                 ?>
-                <input type="text" class="form-control" placeholder="Search a Keyword" name="katakunci" value="<?php echo $katakunci;?>">
+                <input type="text" class="form-control" placeholder="Search a Keyword" name="katakunci"
+                    value="<?php echo $katakunci; ?>">
             </div>
             <div class="col-2">
                 <input type="submit" class="btn btn-success" name="submit" value="Search">
@@ -68,43 +69,78 @@ include("../config.php");
                 $no = 1;
                 $katakunci = "";
 
-                $query = "SELECT * FROM halaman";
-                    $sql = mysqli_query($db, $query);
-                    $no = 1;
+                $batas = 5;
+				$halaman = isset($_GET['halaman'])?(int)$_GET['halaman'] : 1;
+				$halaman_awal = ($halaman>1) ? ($halaman * $batas) - $batas : 0;	
+ 
+				$previous = $halaman - 1;
+				$next = $halaman + 1;
+				
+				$data = mysqli_query($db,"select * from halaman");
+				$jumlah_data = mysqli_num_rows($data);
+				$total_halaman = ceil($jumlah_data / $batas);
+ 
+				$sql = mysqli_query($db,"select * from halaman limit $halaman_awal, $batas");
+				$nomor = $halaman_awal+1;
+                $no = 1;
 
-                if(isset($_POST['submit'])) {
-                    $katakunci = $_POST['katakunci'];                    
+
+                if (isset($_POST['submit'])) {
+                    $katakunci = $_POST['katakunci'];
                     $query = "SELECT * FROM halaman  where judul like '%$katakunci%'";
                     $sql = mysqli_query($db, $query);
                     $no = 1;
                 }
-                    
 
-                
 
-                while ($row = mysqli_fetch_array($sql)) {?>
-                <tr>
-                    <td>
-                        <?php echo $no; ?>
-                    </td>
-                    <td>
-                        <?php echo $row['judul']; ?>
-                    </td>
-                    <td>
-                        <?php echo $row['judul']; ?>
-                    </td>
-                    <td>
-                        <div class="g-4">
-                            <button class="btn btn-warning">Edit</button>
-                            <a href="index.php?operation=delete&id=<?php echo $row['id']?>" onclick="return confirm('Yakin mau hapus data ini?')">
-                                <button class="btn btn-danger">Delete</button>
-                            </a>
-                        </div>
-                    </td>
+
+
+                while ($row = mysqli_fetch_array($sql)) { ?>
+                    <tr>
+                        <td>
+                            <?php echo $no; ?>
+                        </td>
+                        <td>
+                            <?php echo $row['judul']; ?>
+                        </td>
+                        <td>
+                            <?php echo $row['kutipan']; ?>
+                        </td>
+                        <td>
+                            <div class="g-4">
+                                <a href="input_page.php?operation=update&id=<?php echo $row['id'] ?>">
+                                    <button class="btn btn-warning">Edit</button>
+                                </a>
+                                <a href="index.php?operation=delete&id=<?php echo $row['id'] ?>"
+                                    onclick="return confirm('Yakin mau hapus data ini?')">
+                                    <button class="btn btn-danger">Delete</button>
+                                </a>
+                            </div>
+                        </td>
                     </tr>
-                    <?php $no++;} ?>
+                    <?php $no++;
+                } ?>
             </tbody>
         </table>
+
+        <nav>
+			<ul class="pagination justify-content-center mt-5">
+				<li class="page-item">
+					<a class="page-link" <?php if($halaman > 1){ echo "href='?halaman=$previous'"; } ?>>Previous</a>
+				</li>
+				<?php 
+				for($x=1;$x<=$total_halaman;$x++){
+					?> 
+					<li class="page-item"><a class="page-link" href="?halaman=<?php echo $x ?>"><?php echo $x; ?></a></li>
+					<?php
+				}
+				?>				
+				<li class="page-item">
+					<a  class="page-link" <?php if($halaman < $total_halaman) { echo "href='?halaman=$next'"; } ?>>Next</a>
+				</li>
+			</ul>
+		</nav>
+
     </div>
 
     <!-- Footer -->
--- admin/input_page.php
+++ admin/input_page.php
@@ -6,51 +6,74 @@ include("../config.php");
 <?php include("../resource/components/header.php") ?>
 <div class="container p-4">
 
-<?php 
+    <?php
 
-$judul = "";
-$kutipan = "";
-$isi = "";
-$error = "";
-$success = "";
+    (isset($_GET['operation'])?$btn_type = "Update":$btn_type = "Insert"); 
 
-if(isset($_POST['submit'])) {
-    $judul = $_POST['judul'];
-    $kutipan = $_POST['kutipan'];
-    $isi = $_POST['isi'];
+    $judul = "";
+    $kutipan = "";
+    $isi = "";
+    $error = "";
+    $success = "";
 
-    if($judul == "" or $kutipan == "" or $isi == "") {
-        $error = "Data belum dimasukkan dengan benar!";
-    } else {
-        $query = "INSERT INTO halaman(judul, kutipan, isi) values ('$judul', '$kutipan', '$isi')";
-        $goquery = mysqli_query($db, $query);
+    if (isset($_POST['submit'])) {
+        $judul = $_POST['judul'];
+        $kutipan = $_POST['kutipan'];
+        $isi = $_POST['isi'];
+        $id = $_GET['id'];
 
-        if($goquery) {
-            $success = "Data berhasil dimasukkan!";
+
+        if ($judul == "" or $kutipan == "" or $isi == "") {
+            $error = "Data belum dimasukkan dengan benar!";
         } else {
-            $success = "Data gagal dimasukkan!";
+
+            if (isset($_GET['operation'])) {
+                $query = "UPDATE halaman SET judul='$judul', kutipan='$kutipan', isi='$isi' where id='$id'";
+                $goquery = mysqli_query($db, $query);
+
+                if ($goquery) {
+                    $success = "Data berhasil diubah!";
+                } else {
+                    $success = "Data gagal diubah!";
+                }
+            } else {
+                $query = "INSERT INTO halaman(judul, kutipan, isi) values ('$judul', '$kutipan', '$isi')";
+                $goquery = mysqli_query($db, $query);
+    
+                if ($goquery) {
+                    $success = "Data berhasil dimasukkan!";
+                } else {
+                    $success = "Data gagal dimasukkan!";
+                }
+            }
+
         }
+
+        
+
+
     }
- 
-}
 
-?>
+    ?>
 
-<?php
+    <?php
 
-if($error) {?>
+    if ($error) { ?>
 
-<div class="alert alert-danger" role="alert">
-  <?php echo $error;?>
-</div>
+        <div class="alert alert-danger" role="alert">
+            <?php echo $error; ?>
+        </div>
 
-<?php } elseif($success) {?>
-    <div class="alert alert-success" role="alert">
-  <?php echo $success;?>
-</div>
-<?php }?>
+    <?php } elseif ($success) { ?>
+        <div class="alert alert-success" role="alert">
+            <?php echo $success; ?>
+        </div>
+    <?php } ?>
 
-<!-- Content -->
+
+
+
+    <!-- Content -->
     <h1>Admin Page | Input Data</h1>
     <div class="mb-3 row">
         <a href="index.php" class="link-underline link-underline-opacity-0">
@@ -58,35 +81,50 @@ if($error) {?>
 
                 <form action="" method="POST">
                     <form>
+                        <?php if (isset($_GET['operation'])) {
+                            $id = $_GET['id'];
+                            $query = "SELECT * FROM halaman where id='$id'";
+                            $sql = mysqli_query($db, $query);
 
+                            $data = mysqli_fetch_array($sql);
+                            $judul = $data['judul'];
+                            $kutipan = $data['kutipan'];
+                            $isi = $data['isi'];
+
+                            
+                        }
+                        ?>
                         <div class="mb-3 row">
-                            <label for="judul" class="col-sm-2 col-form-label">Judul</label>
+                            <label for="isi" class="col-sm-2 col-form-label">Judul</label>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" id="judul" name="judul" value="<?php echo $judul;?>">
+                                <input type="text" class="form-control" id="judul" name="judul"
+                                    value="<?php echo $judul; ?>">
                             </div>
                         </div>
-                        
+
                         <div class="mb-3 row">
                             <label for="kutipan" class="col-sm-2 col-form-label">Kutipan</label>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" id="kutipan" name="kutipan" value="<?php echo $kutipan;?>">
+                                <input type="text" class="form-control" id="kutipan" name="kutipan"
+                                    value="<?php echo $kutipan; ?>">
                             </div>
                         </div>
 
                         <div class="mb-3 row">
                             <label for="isi" class="col-sm-2 col-form-label">Isi</label>
                             <div class="col-sm-10">
-                                <textarea type="text" class="form-control" id="summernote" name="isi"></textarea></textarea></textarea>
-                                    <?php echo $isi;?>
+                                <textarea type="text" class="form-control" id="summernote" name="isi">
+                                    <?php echo $isi; ?>
                                 </textarea>
                             </div>
                         </div>
 
                         <div class="mb-3 row">
-                        <div class="col-sm-2"></div>
-                        <div class="col-sm-10">
-                            <input type="submit" class="btn btn-primary" id="submit" name="submit" value="Simpan Data">
-                        </div>
+                            <div class="col-sm-2"></div>
+                            <div class="col-sm-10">
+                                <input type="submit" class="btn btn-primary" id="submit" name="submit"
+                                    value="<?php echo $btn_type;?>">
+                            </div>
                         </div>
 
                     </form>
